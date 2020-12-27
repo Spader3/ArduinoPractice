0ed30b5b20694b3f35dcf604c7d1b178ead7f883
Bronnikov Nikita <spader332@gmail.com>
2020-12-28 00:16:46 +0300

    proj reworked to MD5 hash compatibility

--- Arduino/Arduino/MainWindow.xaml.cs
+++ Arduino/Arduino/MainWindow.xaml.cs
@@ -25,59 +25,24 @@ namespace Arduino
     {
         Timer aTimer;
         SerialPort currentPort;
-        byte[] currentFile = new byte[12];
-        byte[] currentByte = new byte[12];
+        byte[] currentFile = File.ReadAllBytes(AppDomain.CurrentDomain.BaseDirectory+"ExampleTextFile.txt");
+        byte[] currentByte = new byte[40];
         private delegate void updateDelegate(string txt);
         public MainWindow()
         {
             InitializeComponent();
             
         }
-        private bool ArduinoDetected()
-        {
-            try
-            {
-                //currentPort.Open();
-                //System.Threading.Thread.Sleep(1000);
-                //// небольшая пауза, ведь SerialPort не терпит суеты
-                //byte[] array = new byte[128];
-                //array[0] = 1;
-                //currentPort.Write(array, 0, array.Length);
-                //string returnMessage = currentPort.ReadLine();
-                //currentPort.Close();
-                //// необходимо чтобы void loop() в скетче содержал код Serial.println("Info from Arduino");
-                //if (!String.IsNullOrEmpty(returnMessage))
-                //{
-                //    return true;
-                //}
-                //else
-                //{
-                //    return false;
-                //}
-                return true;
-            }
-            catch (Exception e)
-            {
-                return false;
-            }
-        }
         private void OnTimedEvent(object sender, ElapsedEventArgs e)
         {
             if (!currentPort.IsOpen) return;
             try // так как после закрытия окна таймер еще может выполнится или предел ожидания может быть превышен
             {
-                // удалим накопившееся в буфере
-                //currentPort.DiscardInBuffer();
-                // считаем последнее значение 
-                var num = 0;
-                byte[] array = new byte[12];
-                num = currentPort.Read(array, 0, 12);
+                currentPort.DiscardInBuffer();
+                string tmp;
+                tmp = currentPort.ReadLine();
+                arduinoResponse.Dispatcher.BeginInvoke(new updateDelegate(updateTextBox), tmp);
 
-                //var num1 = currentPort.Read(array, 1, 12);
-                //var num2 = currentPort.Read(array, 2, 12);
-                var a = currentPort.BytesToRead;
-                currentByte = array;
-                //arduinoResponse.Dispatcher.BeginInvoke(new updateDelegate(updateTextBox), array.ToString());
             }
             catch { }
         }
@@ -89,40 +54,29 @@ namespace Arduino
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            bool ArduinoPortFound = false;
+            //bool ArduinoPortFound = false;
 
             try
             {
                 string[] ports = SerialPort.GetPortNames();
-                foreach (string port in ports)
-                {
-                    currentPort = new SerialPort(port, 9600);
-                    if (ArduinoDetected())
-                    {
-                        ArduinoPortFound = true;
-                        break;
-                    }
-                    else
-                    {
-                        ArduinoPortFound = false;
-                    }
-                }
+                currentPort = new SerialPort(ports[0], 9600);
+                //ArduinoPortFound = true;
             }
             catch { }
 
-            if (ArduinoPortFound == false) return;
-            System.Threading.Thread.Sleep(500); // немного подождем
+            //if (ArduinoPortFound == false) return;
+            System.Threading.Thread.Sleep(300); // немного подождем
 
             currentPort.BaudRate = 9600;
             currentPort.DtrEnable = true;
-            currentPort.ReadTimeout = 1000;
+            //currentPort.ReadTimeout = 1000;
             try
             {
                 currentPort.Open();
             }
             catch { }
 
-            aTimer = new System.Timers.Timer(5000);
+            aTimer = new System.Timers.Timer(3000);
             aTimer.Elapsed += OnTimedEvent;
             aTimer.AutoReset = true;
             aTimer.Enabled = true;
@@ -151,19 +105,13 @@ namespace Arduino
 
         private void btnHash_Click(object sender, RoutedEventArgs e)
         {
-            GOST G = new GOST(256);
-            GOST G512 = new GOST(512);
-            byte[] message = currentFile;
-            byte[] res = G.GetHash(message);
-            byte[] res2 = G512.GetHash(message);
-            string h256 = BitConverter.ToString(res);
-            string h512 = BitConverter.ToString(res2);
+            var str = MD5.Calculate(currentFile);
+            hashSumm.Content = str;
         }
 
         private void btnArduino_Click(object sender, RoutedEventArgs e)
         {
             if (!currentPort.IsOpen) return;
-
             currentPort.Write(currentFile, 0, currentFile.Length);
         }
     }
